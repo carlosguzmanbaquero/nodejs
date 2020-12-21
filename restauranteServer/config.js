ad603484d5a6707dacaa49117536803af66bf2bd
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-12-21 15:33:10 -0500

    autenticacion con facebook

--- restauranteServer/config.js
+++ restauranteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://root:example@localhost:27017/restaurante?authSource=admin'
+    'mongoUrl' : 'mongodb://root:example@localhost:27017/restaurante?authSource=admin',
+    'facebook': {
+        clientId: 'id_cliente',
+        clientSecret: 'clave_cliente'
+    }
 }
