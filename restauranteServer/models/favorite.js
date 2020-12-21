c5d2cd9a9d3a33197d14e3a569a0c73864c493ca
carlos guzman <carlosguzmanbaquero@gmail.com>
2020-12-21 18:16:40 -0500

    se agrega end point de favoritos

---
+++ restauranteServer/models/favorite.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var favoritesSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    dishes: [ 
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Dish'
+        }
+    ]
+}, {
+    timestamps: true
+});
+
+var Favorites = mongoose.model('Favorite', favoritesSchema);
+
+module.exports = Favorites;
